23883e40310098a17856f8477377fdaa65c50dc2
saifalavi <79043069+saifalavi@users.noreply.github.com>
2026-07-26 18:56:28 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+KEYWORDS = [
+    "Bangladesh",
+    "Chattogram",
+    "Chittagong",
+    "Mongla",
+    "oil tanker",
+    "crude oil",
+    "diesel",
+    "petrol",
+    "LPG",
+    "Jamuna Oil",
+    "Padma Oil",
+    "Meghna Petroleum",
+    "BPC"
+]
